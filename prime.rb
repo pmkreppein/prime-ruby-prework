26a929172226972b97653bab6c8875483d6302c3
Peter Kreppein <pmkreppein@gmail.com>
2018-09-20 18:29:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+def check_division(n)
+  flag = true
+  (1..100).step(1) do |x|
+    if n % x == 0 
+      flag = false
+    else 
+      next
+    end
+    return flag
+end
+
+def prime(number)
+  case number
+    when number <= 0 
+      return false
+    when check_division?(number)
+      return true
+    else 
+      return true
+    end
+end
+
+
